7ffa8b5c0c77fc54628a9de1af8f58aa50a39a46
salsabeel95 <huss95salsabeel@gmail.com>
2022-12-20 02:34:50 +0200

    validation refactory with setting up directory in dist

---
+++ src/utilities/setupAssetsDirectory.ts
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import path from 'path'
+import { mkdir, rm, copyFile } from 'fs/promises'
+
+const outputDirctory = 'thumb'
+const imagesDirInDist = path.join(__dirname, '..', outputDirctory)
+const imagesDirInSrc = path.join(__dirname, '..', '..', outputDirctory)
+
+async function setupDirctoryInDist(distPath: string, srcPath: string) {
+  fs.existsSync(distPath) && (await rm(distPath, { recursive: true }))
+  !fs.existsSync(distPath) && (await mkdir(distPath))
+  fs.existsSync(distPath) && fs.readdirSync(srcPath).forEach(async (file) => {
+      await copyFile(
+        path.join(srcPath, file),
+        path.join(imagesDirInDist, file)
+      )
+      console.log(file)
+    })
+}
+try {
+  setupDirctoryInDist(imagesDirInDist, imagesDirInSrc)
+} catch (error) {
+  console.log('error: ', error)
+}
--- src/utilities/validations.ts
+++ src/utilities/validations.ts
@@ -1,81 +1,106 @@
-import fs from "fs";
+import fs from 'fs'
 import sharp from 'sharp'
-import path from "path";
+import path from 'path'
+import { NextFunction, Request,Response } from 'express'
 
 const inputDirctory = 'full'
 const outputDirctory = 'thumb'
 
-export const fileNameValidation = (req: Record<string, any>, res: Record<string, any>, next: Function): void => {
-    const { filename } = req.query
-    if (!filename) {
-        res.status(400).json({ "message": "you must provide image name" })
+const makeOutputDirctoryIfNeeded = async () => {
+  const imagesDir = path.join(__dirname, '..', '..', outputDirctory)
+  if (!fs.existsSync(imagesDir)) {
+    await fs.promises.mkdir(imagesDir)
+  }
+}
+export const fileNameValidation = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const { filename } = req.query
+  if (!filename) {
+    res.status(400).json({ message: 'you must provide image name' })
+  } else {
+    makeOutputDirctoryIfNeeded()
+    const images = fs.readdirSync(inputDirctory)
+    const imagesAvailableNames = images
+      .map((img) => img.replace('.jpg', ''))
+      .join(' ,')
+    if (images.includes(filename + '.jpg')) {
+      next()
     } else {
-        const images = fs.readdirSync(inputDirctory)
-        const imagesAvailableNames = images.map(img => img.replace(".jpg", "")).join(" ,")
-        if (images.includes(filename + '.jpg')) {
-            next()
-        } else {
-            res.status(404).json({ "message": "image name must be one of the following: " + imagesAvailableNames })
-        }
+      res
+        .status(404)
+        .json({
+          message:
+            'image name must be one of the following: ' + imagesAvailableNames,
+        })
     }
+  }
 }
- const doesImgNeedCache=  (width: number, height: number, filename: string): boolean => {
-    const imagesAvailable= fs.readdirSync(outputDirctory)
-    console.log('imagesAvailable: ', imagesAvailable);
-    const newImgName = `${filename}_thumb-w${width}-h${height}.jpg`
-    if (!imagesAvailable.includes(newImgName)) {
-        return true
-    }
-    return false
+const doesImgNeedCache = (
+  width: string,
+  height: string,
+  filename: string
+): boolean => {
+  const imagesAvailable = fs.readdirSync(outputDirctory)
+  // console.log('imagesAvailable: ', imagesAvailable);
+  const newImgName = `${filename}_thumb-w${width}-h${height}.jpg`
+  return !imagesAvailable.includes(newImgName) ? true : false
 }
 
-const resizeImg = async (width: number, height: number, filename: string): Promise<sharp.OutputInfo> => {
-    console.log('resize ...')
-    const inputFile = path.join(inputDirctory, `${filename}.jpg`)
-    const newImgName = `${filename}_thumb-w${width}-h${height}.jpg`
-    const outputFile = path.join(outputDirctory, newImgName)
-    return await sharp(inputFile)
-        .resize({ width, height, fit: 'contain' })
-        .jpeg({ mozjpeg: true })
-        .toFile(outputFile)
+const resizeImg = async (
+  width: number,
+  height: number,
+  filename: string
+): Promise<void> => {
+  console.log(`resize with w=${width} h=${height} ...`)
+  const inputFile = path.join(inputDirctory, `${filename}.jpg`)
+  const newImgName = `${filename}_thumb-w${width}-h${height}.jpg`
+  const outputFile = path.join(outputDirctory, newImgName)
+  await sharp(inputFile)
+    .resize({ width, height, fit: 'contain' })
+    .jpeg({ mozjpeg: true })
+    .toFile(outputFile)
 }
 
-export const widthHeightValidation = async (req: Record<string, any>, res: Record<string, any>, next: Function): Promise<void> => {
-    let { width, height, filename } = req.query
-    if (!height && !width) {
-        // set height and width to default value 200
-        console.log("set height and width to default value 200");
-        width = height = req.query.width = req.query.height = 200
-        doesImgNeedCache(width, height, filename)&& await resizeImg(width, height, filename)
-        next()
+export const widthHeightValidation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const { width = 200, height = 200, filename } = req.query
+
+  if (
+    height &&
+    width &&
+    isFinite(height as unknown as number) &&
+    +height > 0 &&
+    isFinite(width as unknown as number) &&
+    +width > 0
+  ) {
+    // provided valid width or height or both
+    console.log('provided valid width or height or both')
+    doesImgNeedCache(width  as unknown as string , height  as unknown as string, filename  as unknown as string) &&
+      (await resizeImg(+width, +height, filename as unknown as string))
+    next()
+  } else {
+    // only one of them provided
+    if (
+      ((!isFinite(height as unknown as number) || height <= 0) && height) ||
+      (width && (!isFinite(width as unknown as number) || width <= 0))
+    ) {
+      // provided invalid width or height or both
+      console.log('provided invalid width or height or both')
+      res
+        .status(400)
+        .json({ message: 'dimensions must be a postive number greater than 0' })
     }
     else {
-        if (height && width && isFinite(height) && +height > 0 && isFinite(width) && +width > 0) {
-            // resize with width and height
-            console.log('resize with width and height ')
-            doesImgNeedCache(width, height, filename)&& await resizeImg(+width, +height, filename)
-            next()
-        }
-        else {
-            // only one of them provided
-            if (height && isFinite(height) && height > 0) {
-                // resize with height , width =200
-                console.log('resize with height and width will be 200 ')
-                width = req.query.width = 200
-                doesImgNeedCache(width, height, filename)&& await resizeImg(width, +height, filename)
-                next()
-            } else if ((!isFinite(height) || height <= 0) && height) {
-                res.status(400).json({ "message": "height must be a postive number greater than 0" })
-            }
-            else if (width && isFinite(width) && width > 0) {
-                // resize with width , height =200
-                console.log('resize with width and height will be 200 ')
-                height = req.query.height = 200
-                doesImgNeedCache(width, height, filename)&& await resizeImg(+width, height, filename)
-                next()
-            } else if (width && (!isFinite(width) || width <= 0)) {
-                res.status(400).json({ "message": "width must be a postive number greater than 0" })
-            }
-        }
+        // provided valid width or height
+        console.log('provided valid width or height ')
+        doesImgNeedCache(width as unknown as string, height  as unknown as string, filename  as unknown as string) && await resizeImg(+width, +height, filename  as unknown as string)
+        next()
     }
+  }
 }
